38ca52f1eef1b4a6a272fa511476fc94013918e9
Adib <mansuri.adib25@gmail.com>
2022-08-28 05:05:28 +0200

    add score

--- script.js
+++ script.js
@@ -3,7 +3,12 @@
 const score0el = document.querySelector('#score--0');
 const score1el = document.getElementById('score--1');
 const dieseel = document.querySelector('.dice');
-
-score0el.textContent = 0;
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold')(
+  (score0el.textContent = 0)
+);
 score1el.textContent = 0;
 dieseel.classList.add('hidden');
+
+//rolling dies functionality
