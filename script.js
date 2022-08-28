0dbebdae59a0c10ab42da87df6b0bbbc29aca39e
Adib <mansuri.adib25@gmail.com>
2022-08-28 04:34:45 +0200

    start project

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+//selecting element
+const score0el = document.querySelector('#score--0');
+const score1el = document.getElementById('score--1');
+const dieseel = document.querySelector('.dice');
+
+score0el.textContent = 0;
+score1el.textContent = 0;
+dieseel.classList.add('hidden');
