c69db4258463b7035beb94c26466b31eeed9126a
Adib <mansuri.adib25@gmail.com>
2022-08-29 22:47:28 +0200

    ADD ELE

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const score0el = document.querySelector('#score--0');
 const score1el = document.getElementById('score--1');
 const dieseel = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold')(
   (score0el.textContent = 0)
 );
 score1el.textContent = 0;
