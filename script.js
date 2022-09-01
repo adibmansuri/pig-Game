dd7e1fe405ff86cc91aaeb217582b00f0d1bfd71
Adib <mansuri.adib25@gmail.com>
2022-09-02 01:28:43 +0200

    Add hidden class

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 //selecting element
 const score0el = document.querySelector('#score--0');
 const score1el = document.getElementById('score--1');
-const dieseel = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold')(
-  (score0el.textContent = 0)
-);
+// const dieseel = document.querySelector('.dice');
+// const btnNew = document.querySelector('.btn--new');
+// const btnRoll = document.querySelector('.btn--roll');
+// const btnHold = document.querySelector('.btn--hold')(
+//   (score0el.textContent = 0)
+// );
+//starting condition
+score0el.textContent = 0;
 score1el.textContent = 0;
 dieseel.classList.add('hidden');
 
