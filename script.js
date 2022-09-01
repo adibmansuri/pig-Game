714c494a1cf933488879f4d108c24016bae99a5c
Adib <mansuri.adib25@gmail.com>
2022-09-02 01:50:00 +0200

    Add dice random

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 //selecting element
 const score0el = document.querySelector('#score--0');
 const score1el = document.getElementById('score--1');
-// const dieseel = document.querySelector('.dice');
-// const btnNew = document.querySelector('.btn--new');
-// const btnRoll = document.querySelector('.btn--roll');
+const dieseel = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
 // const btnHold = document.querySelector('.btn--hold')(
 //   (score0el.textContent = 0)
 // );
@@ -14,3 +14,11 @@ score1el.textContent = 0;
 dieseel.classList.add('hidden');
 
 //rolling dies functionality
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  //display dice
+  dieseel.classList.remove('hidden');
+  dieseel.src = `dice-${dice}.png`;
+});
