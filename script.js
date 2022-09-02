5e3e48531e6b5adfb3c475624cc3933069e0ad97
Adib <mansuri.adib25@gmail.com>
2022-09-02 02:38:30 +0200

    Add Active player js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 //selecting element
 const score0el = document.querySelector('#score--0');
 const score1el = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const dieseel = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,6 +15,9 @@ score0el.textContent = 0;
 score1el.textContent = 0;
 dieseel.classList.add('hidden');
 
+let currentScore = 0;
+let activePlayer = 0;
+
 //rolling dies functionality
 btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -21,4 +26,13 @@ btnRoll.addEventListener('click', function () {
   //display dice
   dieseel.classList.remove('hidden');
   dieseel.src = `dice-${dice}.png`;
+
+  //if 1
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
 });
