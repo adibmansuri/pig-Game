15f05ce778b2e10c36da965cad2e77737ba526dc
Adib <mansuri.adib25@gmail.com>
2022-09-10 13:24:32 +0200

    ad hold btn

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const current1El = document.getElementById('current--1');
 const dieseel = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
-// const btnHold = document.querySelector('.btn--hold')(
+const btnHold = document.querySelector('.btn--hold');
 //   (score0el.textContent = 0)
 // );
 //starting condition
@@ -17,9 +17,17 @@ score0el.textContent = 0;
 score1el.textContent = 0;
 dieseel.classList.add('hidden');
 
+const score = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchplayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 //rolling dies functionality
 btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -36,10 +44,18 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchplayer();
   }
+});
+btnHold.addEventListener('click', function () {
+  //add curent score toactive player score
+  score[activePlayer] += currentScore;
+  // scores[1] = scores[1] + currentScore
+
+  document.getElementById(`score--${activePlayer}`).textContent =
+    score[activePlayer];
+  //check if score is <=100
+  //finish the game if >100
+  //if not switch to next player
+  switchplayer();
 });
